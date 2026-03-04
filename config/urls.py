c1a999a7a5f66f79e486ccebbd32628608222f2f
Globass-prog <gloiremukekwa6@gmail.com>
2026-03-04 13:25:24 +0200

    Add home view for Project Manager API

--- config/urls.py
+++ config/urls.py
@@ -14,7 +14,8 @@ from kanban.admin_dashboard import admin_dashboard_view, dashboard_stats_api
 
 # Page d'accueil simple
 
-
+def home(request):
+    return HttpResponse("Project Manager API is running 🚀")
 
 urlpatterns = [
     # 🔥 Dashboard custom AVANT admin
