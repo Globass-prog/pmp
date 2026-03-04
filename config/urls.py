e0163c672df2b9409e55a73537bd065f3130076e
Globass-prog <gloiremukekwa6@gmail.com>
2026-03-04 13:44:07 +0200

    Import redirect from django.shortcuts

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path
+from django.shortcuts import redirect
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
